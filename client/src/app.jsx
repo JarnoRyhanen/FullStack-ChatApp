d58c133f758bf58920e63d931c59b1aa0cb13142
Jarno02 <jarno.ryhanen02@gmail.com>
2023-01-27 14:09:54 +0200

    Added Css styles, minor changes

--- client/src/app.jsx
+++ client/src/app.jsx
@@ -1,11 +1,28 @@
-import React from 'react'
+import React from 'react';
+import { StreamChat } from "stream-chat";
+import { Chat } from "stream-chat-react";
+import Cookies from "universal-cookie";
+
+import { ChannelContainer, ChannelListContainer} from './components';
+
+
+const apiKey = "bztvbcd9dbg3";
+
+const client = StreamChat.getInstance(apiKey);
 
 const app = () => {
-  return (
-    <div>
-      <h1>Messenger Pager chat App</h1>
-    </div>
-  )
+    return (
+        <div className='app__wrapper'>
+            <Chat client={client} theme="team light">
+                <ChannelListContainer
+
+                />
+                <ChannelContainer
+
+                />
+            </Chat>
+        </div>
+    )
 }
 
 export default app
