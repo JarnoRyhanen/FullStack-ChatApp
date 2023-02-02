18226e9be9c266231c005f756cf9194259a0dbd8
Jarno02 <jarno.ryhanen02@gmail.com>
2023-02-02 14:05:11 +0200

    Login and signin finished

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,7 +1,9 @@
 const { connect } = require("getstream");
 const bcrypt = require("bcrypt");
-const StreamChat = require("stream-chat");
+const StreamChat = require("stream-chat").StreamChat;
 const crypto = require("crypto");
+
+require("dotenv").config();
 
 const api_key = process.env.STREAM_API_KEY;
 const api_secret = process.env.STREAM_API_SECRET;
@@ -30,21 +32,21 @@ const signup = async (req, res) => {
 
 const login = async (req, res) => {
     try {
-        const { userName, password } = req.body;
+        const { username, password } = req.body;
 
         const serverClient = connect(api_key, api_secret, app_id);
         const client = StreamChat.getInstance(api_key, api_secret);
-        const { users } = await client.queryUsers({ name: userName });
+        const { users } = await client.queryUsers({ name: username });
 
         if (!users.length) return res.status(400).json({ message: "User not found!" });
         const success = await bcrypt.compare(password, users[0].hashedPassword);
 
-        const token = serverClient.createUserToken(userId);
+        const token = serverClient.createUserToken(users[0].id);
 
         if (success) {
             res.status(200).json({ token, fullname: users[0].fullName, username, userId: users[0].id });
         } else {
-            res.status(500).json({ message: "Incorrect password" });
+            res.status(503).json({ message: "Incorrect password" });
         }
 
     } catch (error) {
